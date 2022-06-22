a3a3f926e18454e5d5860d165dde5dbc776711a2
Tri Doan <tridoan1998@gmail.com>
2022-06-22 01:15:14 -0700

    chore: adding classes

---
+++ Inheritance.java
@@ -0,0 +1,30 @@
+//subclass and superclass
+
+
+class Car {
+
+    //constructor
+    public Car() {
+        String branch;
+    }
+
+    //setter
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    //getter
+    public String getBranch(){
+        return this.branch;
+    }
+
+    //some methods
+    public void print_Cars(){
+        for(int i = 0; i < )
+    }
+    
+    //main 
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
