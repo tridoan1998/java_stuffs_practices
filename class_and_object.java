5e07c481702d2a6c8b9257dd8a562b5db70e26ca
Tri Doan <tridoan1998@gmail.com>
2022-06-21 20:07:17 -0700

    chore: finised getter and setter

--- class_and_object.java
+++ class_and_object.java
@@ -1,18 +1,33 @@
 class Main {
     int x;
 
-    static void prining_hello(){
-        System.out.println("prining_hello!");
+    //constructor
+    public Main(){
+        
+    }
+    
+    //setter
+    public void setX(int x) {
+        this.x = x;
     }
 
-    public Main(){
-        x = 10;
+    //getter
+    public int getX() {
+        return this.x;
+    }
+     
+
+
+    static void prining_hello(){
+        System.out.println("prining_hello!");
     }
 
     public static void main (String [] args) {
         Main object1 = new Main();
         System.out.println(object1.x);
-        object1.prining_hello();
+        object1.setX(20);
+        System.out.println(object1.getX());
+        //object1.prining_hello();
 
     }
 }
