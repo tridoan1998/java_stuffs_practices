1dce66ec2e0815dd3a04b04b08bde1835dd564a3
Tri Doan <tridoan1998@gmail.com>
2022-06-25 19:57:36 -0700

    chore: getting client server to talk to each other

--- MyClient.java
+++ MyClient.java
@@ -1,22 +1,52 @@
+// Client2 class that
+// sends data and receives also
+  
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-
-public class MyClient {
-
-    public static void main(String[] args){
-        try{
-            Socket s = new Socket("localhost", 6666);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            Scanner myObj = new Scanner(System.in);
-            System.out.print("Enter username: ");
-            String userName = myObj.nextLine();
-            dout.writeUTF(userName);
-            dout.flush();
-            dout.close();
-            s.close();
-        }catch(Exception e) {
-            System.out.println(e);
+  
+class MyClient {
+  
+    public static void main(String args[])
+        throws Exception
+    {
+  
+        // Create client socket
+        Socket s = new Socket("localhost", 888);
+  
+        // to send data to the server
+        DataOutputStream dos
+            = new DataOutputStream(
+                s.getOutputStream());
+  
+        // to read data coming from the server
+        BufferedReader br
+            = new BufferedReader(
+                new InputStreamReader(
+                    s.getInputStream()));
+  
+        // to read data from the keyboard
+        BufferedReader kb
+            = new BufferedReader(
+                new InputStreamReader(System.in));
+        String str, str1;
+  
+        // repeat as long as exit
+        // is not typed at client
+        while (!(str = kb.readLine()).equals("exit")) {
+  
+            // send to the server
+            dos.writeBytes(str + "\n");
+  
+            // receive from the server
+            str1 = br.readLine();
+  
+            System.out.println(str1);
         }
+  
+        // close connection.
+        dos.close();
+        br.close();
+        kb.close();
+        s.close();
     }
 }
