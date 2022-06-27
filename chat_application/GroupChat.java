4d55e4bb8eb55560b9665262836d5076a0163f79
Tri Doan <tridoan1998@gmail.com>
2022-06-26 22:31:44 -0700

    chore: understanding what String[] args is

--- chat_application/GroupChat.java
+++ chat_application/GroupChat.java
@@ -3,5 +3,10 @@ import java.net.*;
 import java.util.*;
 
 public class GroupChat {
-    
+    private static final String TERMINATE = "Exit";
+    static String name;
+    static volatile boolean finished = false;
+    public static void main(String[] args) {
+        System.out.println(args.length);
+    }
 }
