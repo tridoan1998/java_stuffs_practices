bc441f7848ea04d4c8e820ddc966b49dc3a99d18
Tri Doan <tridoan1998@gmail.com>
2022-07-11 14:03:44 -0700

    chore: adding comments for the class

---
+++ bank.management.system/JRadioButton.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.swing.*;
+import java.awt.event.*;
+
+
+//creting a class that implement a RadioButton to ask user to click either the person is male or female
+class Demo extends JFrame {
+    JRadioButton radio_button gender;
+
+    Demo() {
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("hello from main function...");
+    }
+}
