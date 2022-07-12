a83caaf09d59818723c9825627027e30478c93db
Tri Doan <tridoan1998@gmail.com>
2022-07-11 21:52:16 -0700

    chore: making RadioButtonExample

--- bank.management.system/RadioButtonExample.java
+++ bank.management.system/RadioButtonExample.java
@@ -1,17 +1,17 @@
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;    
 import java.awt.event.*;
 
 
 //creting a class that implement a RadioButton to ask user to click either the person is male or female
-class Demo extends JFrame {
-    JRadioButton radio_button gender;
+class RadioButtonExample extends JFrame {
+    //JRadioButton radio_button gender;
 
-    Demo() {
-
+    RadioButtonExample() {
+        
     }
     
     public static void main(String[] args) {
-        System.out.println("hello from main function...");
+        new RadioButtonExample();    
     }
 }
