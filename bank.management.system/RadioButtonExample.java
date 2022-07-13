6c544229f479b5567d75d1384cde125ed760c6ba
Tri Doan <tridoan1998@gmail.com>
2022-07-12 17:38:06 -0700

    chore: finished actionPerformed method to get inut button choosen from users

--- bank.management.system/RadioButtonExample.java
+++ bank.management.system/RadioButtonExample.java
@@ -5,14 +5,14 @@ import java.awt.event.*;
 
 //creting a class that implement a RadioButton to ask user to click either the person is male or female
 class RadioButtonExample extends JFrame implements ActionListener {
-    //JRadioButton radio_button gender;
     JFrame frame;
     JButton button;
+    JRadioButton r1, r2;
 
     RadioButtonExample() {
         frame = new JFrame();
-        JRadioButton r1 = new JRadioButton("A) Male");
-        JRadioButton r2 = new JRadioButton("B) Female");
+        r1 = new JRadioButton("A) Male");
+        r2 = new JRadioButton("B) Female");
 
         r1.setBounds(100, 100, 100, 100); 
         r2.setBounds(200, 100, 100, 100);
@@ -23,7 +23,7 @@ class RadioButtonExample extends JFrame implements ActionListener {
 
         //creating button
         button = new JButton("Click");
-        button.setBounds(100, 150, 200, 200);
+        button.setBounds(100, 200, 70, 40);
         button.addActionListener(this);
         
         frame.add(button);
@@ -33,11 +33,24 @@ class RadioButtonExample extends JFrame implements ActionListener {
         frame.setSize(300, 300);
         frame.setLayout(null);
         frame.setVisible(true);
+
     }
 
     public void actionPerformed(ActionEvent e){
-        JOptionPane.showMessageDialog(this,"You are Male.");  
+        if (r1.isSelected())
+        {
+            JOptionPane.showMessageDialog(this,"You are Male.");  
+        }
+        else if (r2.isSelected()) 
+        {
+            JOptionPane.showMessageDialog(this,"You are Female.");  
+        }
+        else if (!r1.isSelected() && !r2.isSelected())
+        {
+            JOptionPane.showMessageDialog(this,"Please select option.");  
+        }
     }
+
     
     public static void main(String[] args) {
         new RadioButtonExample();    
